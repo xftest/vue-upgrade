546138892467169ff51e6f40af0d384c493c03de
fengxiangfei <2778824262@qq.com>
2023-01-28 17:54:32 +0800

    fi

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,17 @@
 var path = require("path");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+
+const smp = new SpeedMeasurePlugin({}); //测量构建速度的插件
 
 module.exports = {
-  configureWebpack: (config) => {
-    Object.assign(config, {
-      resolve: {
-        alias: {
-          src: path.resolve(__dirname, "./src"),
-          assets: path.resolve(__dirname, "./src/assets"),
-          components: path.resolve(__dirname, "./src/components"),
-        }, // 别名配置
-      },
-    });
-  },
+  configureWebpack: smp.wrap({
+    disable: !process.env.MEASURE, // 为true时构建日志不再打印
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, "./src"),
+        assets: path.resolve(__dirname, "./src/assets"),
+        components: path.resolve(__dirname, "./src/components"),
+      }, // 别名配置
+    },
+  }),
 };
